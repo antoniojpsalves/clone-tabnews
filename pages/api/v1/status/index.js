9948c3f1f1ffcd64c93590813b598caf26d107cd
Antonio <antonio.jpsalves@gmail.com>
2025-10-03 19:38:24 -0300

    :sparkles: feat: faz o arquivo `database.js` mais robusto ao lidar com erros em execucao de queries

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,19 +3,33 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  // Verificando versão do Postgres
+  const databaseVersionResult = await database.query(`SHOW server_version;`);
+
   // Verificando max conections do postgres
-  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+  const databaseMaxConnectionsResult = await database.query(
+    `SHOW max_connections;`,
+  );
 
   // Verificando número de conexões ativas
-  const activeConnectionsResult = await database.query(
-    `SELECT COUNT(*) FROM pg_stat_activity;`,
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
 
   // Existe um padrão para devolver objetos em API REST chamado snake case.
   response.status(200).json({
     updated_at: updatedAt,
-    max_connections: Number(maxConnectionsResult.rows[0].max_connections),
-    active_connections: Number(activeConnectionsResult.rows[0].count),
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnectionsResult.rows[0].max_connections,
+        ),
+        opened_connections: databaseOpenedConnectionsResult.rows[0].count,
+      },
+    },
   });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,20 +3,25 @@ test("GET to /api/v1/status return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
-
-  // Verificando se a data foi recebida
-  expect(responseBody.updated_at).toBeDefined();
+  // console.log(responseBody);
 
   // Convertendo a string de volta para poder conferir a data ( barrar null por exemplo )
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
 
-  expect(responseBody.max_connections).toBeDefined();
-  expect(typeof responseBody.max_connections).toBe("number");
-  expect(responseBody.max_connections).toBeGreaterThan(0);
+  // Verificando se a versão é a 16
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  expect(responseBody.active_connections).toBeDefined();
-  expect(typeof responseBody.active_connections).toBe("number");
-  expect(responseBody.active_connections).toBeGreaterThan(0);
+  // Verificações de max connections
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    "number",
+  );
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  // Verificações de active connections
+  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
+    "number",
+  );
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
