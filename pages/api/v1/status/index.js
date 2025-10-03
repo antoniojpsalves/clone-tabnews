f1c6de637d044e5e0a00bb94ab4e30e19128d35b
Antonio <antonio.jpsalves@gmail.com>
2025-10-03 18:08:13 -0300

    :sparkles: feat: dados de status - minha versao

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,21 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  // Verificando max conections do postgres
+  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+
+  // Verificando número de conexões ativas
+  const activeConnectionsResult = await database.query(
+    `SELECT COUNT(*) FROM pg_stat_activity;`,
+  );
+
+  // Existe um padrão para devolver objetos em API REST chamado snake case.
+  response.status(200).json({
+    updated_at: updatedAt,
+    max_connections: Number(maxConnectionsResult.rows[0].max_connections),
+    active_connections: Number(activeConnectionsResult.rows[0].count),
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,22 @@
-test("GET to /api/v1/status returns status ok", async () => {
-  const { status } = await fetch("http://localhost:3000/api/v1/status");
+test("GET to /api/v1/status return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 
-  expect(status).toBe(200);
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  // Verificando se a data foi recebida
+  expect(responseBody.updated_at).toBeDefined();
+
+  // Convertendo a string de volta para poder conferir a data ( barrar null por exemplo )
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.max_connections).toBeDefined();
+  expect(typeof responseBody.max_connections).toBe("number");
+  expect(responseBody.max_connections).toBeGreaterThan(0);
+
+  expect(responseBody.active_connections).toBeDefined();
+  expect(typeof responseBody.active_connections).toBe("number");
+  expect(responseBody.active_connections).toBeGreaterThan(0);
 });
